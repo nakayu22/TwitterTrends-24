2f86c6ddefc66e25814fb54c3bc06862146e83d6
nakayu22 <nakakita.yudai@g.wakayama-u.jp>
2022-03-11 00:14:58 +0900

    add/delete

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template
-from database import db_session, get_trends
+from database import db_session, get_trends, store_ttrends, delete_ttrends
 from apscheduler.schedulers.background import BackgroundScheduler
-from getTrends import store_ttrends
 
 
 app = Flask(__name__)
 
 sched = BackgroundScheduler(daemon=True, timezone="Asia/Tokyo")
 sched.add_job(store_ttrends, 'cron', hour='0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22')
+sched.add_job(delete_ttrends, 'cron', hour='0', day_of_week='fri')
 sched.start()
 
 
--- database.py
+++ database.py
@@ -1,9 +1,22 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+import tweepy
+import datetime
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+API_Key     = os.getenv("API_KEY")
+API_Sec     = os.getenv("API_SEC")
+Token       = os.getenv("TOKEN")
+Token_Sec   = os.getenv("TOKEN_SEC")
 
 
 engine = create_engine('sqlite:///test.db')
+# DB_URI = os.getenv("DB_URI")
+# engine = create_engine(DB_URI)
 db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
 Base = declarative_base()
 Base.query = db_session.query_property()
@@ -28,3 +41,45 @@ def get_trends():
         trends.append(trend)
 
     return trends
+
+
+def store_ttrends():
+    # Twitterオブジェクトの生成
+    auth = tweepy.OAuthHandler(API_Key, API_Sec)
+    auth.set_access_token(Token, Token_Sec)
+    api = tweepy.API(auth)
+
+    #日本のWOEID
+    woeid = 23424856
+    #トレンド一覧取得
+    ttrends = api.get_place_trends(woeid)
+    l = ttrends[0]['trends']
+    trends = []
+    for d in l:
+        trends.append(list(d.values()))
+
+    # 現在の日本の時刻
+    tz_jst = datetime.timezone(datetime.timedelta(hours=9))
+    now = datetime.datetime.now(tz=tz_jst)
+
+    t = models.Trends(trends=trends, created=now)
+    db_session.add(t)
+    db_session.commit()
+
+    return
+
+
+def delete_ttrends():
+    tz_jst = datetime.timezone(datetime.timedelta(hours=9))
+    now = datetime.datetime.now(tz=tz_jst)
+    last = now + datetime.timedelta(days=-31)
+
+    data = db_session.query(models.Trends).filter(models.Trends.created < now).all()
+    db_session.delete(data)
+    db_session.commit()
+
+    return
+
+
+if __name__ == "__main__":
+    init_db()
