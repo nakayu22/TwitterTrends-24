0eea7ed6adbdf8c9512ec6ea29ede1c3c353418b
nakayu22 <nakakita.yudai@g.wakayama-u.jp>
2022-03-16 16:00:02 +0900

    make it better
    make it better

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # TwitterTrends-24
+ツイッターで日本のトレンド入りした直近24時間（2時間ごと）の話題を表示しています．
+
+https://twittertrends-24.herokuapp.com/
--- clock.py
+++ clock.py
@@ -1,13 +1,14 @@
 from database import store_ttrends, delete_ttrends
 from apscheduler.schedulers.blocking import BlockingScheduler
+import urllib.request
 
 
 def keep_alive():
-    print("keep_alive")
+    print(urllib.request.Request('https://twittertrends-24.herokuapp.com/'))
 
 
 sched = BlockingScheduler(timezone="Asia/Tokyo")
-sched.add_job(keep_alive, 'interval', minutes=25)
+sched.add_job(keep_alive, 'interval', minutes=20)
 sched.add_job(store_ttrends, 'cron', hour='0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22')
 sched.add_job(delete_ttrends, 'cron', hour='23', day_of_week='fri')
 sched.start()
