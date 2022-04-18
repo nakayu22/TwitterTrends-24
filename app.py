3e85dbc71c04b735708b387f9b781f85ca743429
nakayu22 <nakakita.yudai@g.wakayama-u.jp>
2022-04-18 15:05:21 +0900

    fix

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 from flask import Flask, render_template
 from database import db_session, get_trends
-# from database import db_session, get_trends, store_ttrends, delete_ttrends
-# from apscheduler.schedulers.background import BackgroundScheduler
+from database import db_session, get_trends, store_ttrends, delete_ttrends
+from apscheduler.schedulers.background import BackgroundScheduler
 
 
 app = Flask(__name__)
 
-# sched = BackgroundScheduler(daemon=True, timezone="Asia/Tokyo")
-# sched.add_job(store_ttrends, "cron", hour="0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22")
-# sched.add_job(delete_ttrends, "cron", hour="23", day_of_week="fri")
-# sched.start()
+sched = BackgroundScheduler(timezone="Asia/Tokyo")
+sched.add_job(store_ttrends, "cron", hour="0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23")
+sched.add_job(delete_ttrends, "cron", hour="23", minute="30", day_of_week="fri")
+sched.start()
 
 
 @app.route("/")
