217ef1777f2bbc5ed28726d8ce6e7cd37a223a41
nakayu22 <nakakita.yudai@g.wakayama-u.jp>
2022-06-07 16:56:59 +0900

    fix

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 # 定期実行
 sched = BackgroundScheduler(timezone="Asia/Tokyo")
 sched.add_job(store_ttrends, "cron", hour="0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23")
-sched.add_job(makeWc, "cron", hour="0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23", second="30")
+sched.add_job(createWc, "cron", hour="0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23", second="30")
 sched.add_job(delete_ttrends, "cron", hour="23", minute="30", day_of_week="fri")
 sched.start()
 
